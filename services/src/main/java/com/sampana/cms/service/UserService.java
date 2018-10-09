a70a79f6a9af187f3640c16411e945144ac93ad0
sudhanshu.c <sudhanshu.c@SLS-CPU-213s.SLS.loc>
2018-10-09 22:56:38 +0530

    user service changes

--- services/src/main/java/com/sampana/cms/service/UserService.java
+++ services/src/main/java/com/sampana/cms/service/UserService.java
@@ -3,22 +3,25 @@ package com.sampana.cms.service;
 
 import org.springframework.stereotype.Service;
 
-
+import com.sampana.cms.dto.UserDTO;
+import com.sampana.cms.model.ResponseDTO;
 import com.sampana.cms.model.User;
 import com.sampana.cms.repository.RoleRepository;
 import com.sampana.cms.repository.UserRepository;
 
 
 
-@Service("userService")
-public class UserService {
+//@Service("userService")
+public interface UserService {
 
-    private UserRepository userRepository;
+    /*private UserRepository userRepository;
     private RoleRepository roleRepository;
 
     public User findUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+*/
+	public ResponseDTO save(UserDTO userDTO);
 
 
 
